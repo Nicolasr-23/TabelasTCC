dd6e015e3180208bf9c33a3bb5cdbae5dbc1c997
Nicolasr-23 <rm93903@estudante.fieb.edu.br>
2025-05-30 09:44:37 -0300

    Tabela Empresa

--- bd_tcc_modelo.sql
+++ bd_tcc_modelo.sql
@@ -50,13 +50,13 @@ CREATE TABLE Aluno
 )
 GO
 INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusAluno)
-VALUES ('Fulano da Silva', 'rm12345@estudante.fieb.edu.br', GETDATE(), 'ATIVO')
+VALUES ('João Oliveira', 'rm12345', 'MTIzNDU2Nzg=', NULL, GETDATE(), 'INATIVO')
 
-INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusUsuario)
-VALUES ('Sicrando de Oliveira', 'rm12345','MTIzNDU2Nzg=', NULL,  GETDATE(), 'INATIVO')
+INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusAluno)
+VALUES ('Cleiton dos Santos', 'rm12345','MTIzNDU2Nzg=', NULL,  GETDATE(), 'ATIVO')
 
-INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusUsuario)
-VALUES ('Beltrana de Sá', 'rm12345', 'MTIzNDU2Nzg=', , NULL, GETDATE(), 'ATIVO')
+INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusAluno)
+VALUES ('Jorge da Silva', 'rm12345', 'MTIzNDU2Nzg=', NULL, GETDATE(), 'ATIVO')
 
 GO
 
@@ -75,18 +75,40 @@ CREATE TABLE Administrador(
 )
 GO
 
-INSERT Aluno (nome, email, senha, foto, dataCadastro, statusAluno)
-VALUES ('Julia da Silva', 'user1@gmail.com', GETDATE(), 'ATIVO')
+INSERT Administrador(nome, email, senha, dataCadastro, statusAdministrador)
+VALUES ('Julia da Silva', 'user1@gmail.com','MTIzNDU2Nzg=', GETDATE(), 'ATIVO')
 
-INSERT Aluno (nome, email, senha, foto, dataCadastro, statusUsuario)
+INSERT Administrador (nome, email, senha, dataCadastro, statusAdministrador)
 VALUES ('José de Oliveira', 'user2gmail.com','MTIzNDU2Nzg=',  GETDATE(), 'INATIVO')
 
-INSERT Aluno (nome, email, senha, foto, dataCadastro, statusUsuario)
-VALUES ('João de Oliveira', 'user3@gmail.com', 'MTIzNDU2Nzg=', GETDATE(), 'ATIVO')
+INSERT Administrador(nome, email, senha, dataCadastro, statusAdministrador)
+VALUES ('Carlos de Oliveira', 'user3@gmail.com', 'MTIzNDU2Nzg=', GETDATE(), 'ATIVO')
 
 SELECT * FROM Administrador
 
 GO
 
+CREATE TABLE Empresa(
+	id_Empresa		INT				IDENTITY,
+	nome			VARCHAR(100)	NOT NULL,
+	senha			VARCHAR(100)	NOT NULL,
+	email			VARCHAR(100)	NOT NULL,
+	cnpj			VARCHAR(18)		UNIQUE NOT NULL,
+	endereço		VARCHAR(300)	NOT NULL,
+	dataCadastro	SMALLDATETIME	NOT NULL,
+	statusEmpresa	VARCHAR(20)		NOT NULL,
 
+	PRIMARY KEY (id_Empresa)
+)
+GO
 
+INSERT Empresa (nome, senha, email, cnpj, endereço, dataCadastro, statusEmpresa)
+VALUES ('InovaTech'), 'MTIzNDU2Nzg=', 'Inovatech@gmail.com', '11.222.333/0001-81', 'Avenida dos Girassóis N°200', GETDATE() , 'INATIVO')
+
+INSERT Empresa (nome, senha, email, cnpj, endereço, dataCadastro, statusEmpresa)
+VALUES ('CodeStorm'), 'MTIzNDU2Nzg=', 'CodeStorm@gmail.com', '12.345.678/0001-95', 'Rua São Benedito N°384', GETDATE() , 'ATIVO')
+
+INSERT Empresa (nome, senha, email, cnpj, endereço, dataCadastro, statusEmpresa)
+VALUES ('Biotec Pharma'), 'MTIzNDU2Nzg=', 'biotecpharma@gmail.com', '98.765.432/0001-10', 'Rua Marechal Deodoro N°490', GETDATE() , 'INATIVO')
+
+SELECT * FROM Empresa
