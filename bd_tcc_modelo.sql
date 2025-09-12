1b447d236f1477a24e7023f880e0f290ba382d25
Nicolasr-23 <rm93903@estudante.fieb.edu.br>
2025-09-12 09:44:43 -0300

    Tabelas do TCC prontas

--- bd_tcc_modelo.sql
+++ bd_tcc_modelo.sql
@@ -1,185 +1,167 @@
-USE master IF EXISTS(select * from sys.databases where name='bd_seuprojeto') 
-DROP DATABASE bd_seuprojeto
-GO 
--- CRIAR UM BANCO DE DADOS
-CREATE DATABASE bd_seuprojeto
+USE master;
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'bd_ZeroUm') 
+BEGIN
+    DROP DATABASE bd_ZeroUm;
+END;
 GO
--- ACESSAR O BANCO DE DADOS
-USE bd_seuprojeto
+ 
+-- Criar o banco de dados
+CREATE DATABASE bd_ZeroUm;
 GO
-
+ 
+-- Acessar o banco de dados
+USE bd_ZeroUm;
+GO
+ 
+-- Criar a tabela Usuario
 CREATE TABLE Usuario
 ( 
    id            INT			IDENTITY,
    nome          VARCHAR(100)	NOT NULL,
    email         VARCHAR(100)	UNIQUE NOT NULL,
    senha         VARCHAR(100)	NOT NULL,
-   nivelAcesso   VARCHAR(10)    NULL, -- ADMIN ou ALUNO ou EMPRESA
+   nivelAcesso   VARCHAR(10)    NULL, -- ADMIN, ALUNO ou EMPRESA
    foto			 VARBINARY(MAX) NULL,
    dataCadastro	 SMALLDATETIME	NOT NULL,
-   statusUsuario VARCHAR(20)    NOT NULL, -- ATIVO ou INATIVO ou TROCAR_SENHA
-
+   statusUsuario VARCHAR(20)    NOT NULL, -- ATIVO, INATIVO ou TROCAR_SENHA
+ 
    PRIMARY KEY (id)
-)
+);
 GO
-INSERT Usuario (nome, email, senha, nivelAcesso, foto, dataCadastro, statusUsuario)
-VALUES ('Fulano da Silva', 'fulano@email.com.br', 'MTIzNDU2Nzg=', 'ADMIN', NULL, GETDATE(), 'ATIVO')
-INSERT Usuario (nome, email, senha, nivelAcesso, foto, dataCadastro, statusUsuario)
-VALUES ('Beltrana de Sá', 'beltrana@email.com.br', 'MTIzNDU2Nzg=', 'USER', NULL, GETDATE(), 'ATIVO')
-INSERT Usuario (nome, email, senha, nivelAcesso, foto, dataCadastro, statusUsuario)
-VALUES ('Sicrana de Oliveira', 'sicrana@email.com.br', 'MTIzNDU2Nzg=', 'USER', NULL, GETDATE(), 'INATIVO')
-INSERT Usuario (nome, email, senha, nivelAcesso, foto, dataCadastro, statusUsuario)
-VALUES ('Ordnael Zurc', 'ordnael@email.com.br', 'MTIzNDU2Nzg=', 'USER', NULL, GETDATE(), 'TROCAR_SENHA')
+ 
+-- Inserir dados na tabela Usuario
+INSERT INTO Usuario (nome, email, senha, nivelAcesso, foto, dataCadastro, statusUsuario)
+VALUES 
+('Fulano da Silva', 'fulano@email.com.br', 'MTIzNDU2Nzg=', 'ADMIN', NULL, GETDATE(), 'ATIVO'),
+('Beltrana de Sá', 'beltrana@email.com.br', 'MTIzNDU2Nzg=', 'ALUNO', NULL, GETDATE(), 'ATIVO'),
+('Sicrana de Oliveira', 'sicrana@email.com.br', 'MTIzNDU2Nzg=', 'ALUNO', NULL, GETDATE(), 'INATIVO'),
+('Ordnael Zurc', 'ordnael@email.com.br', 'MTIzNDU2Nzg=', 'ALUNO', NULL, GETDATE(), 'TROCAR_SENHA');
 GO
-
-SELECT * FROM Usuario
-
---Tabelas:Aluno, administrador, empresa, vaga, perfil do aluno.
-
+ 
+-- Selecionar dados da tabela Usuario
+SELECT * FROM Usuario;
+ 
+-- Criar a tabela Aluno
 CREATE TABLE Aluno
 (
-	id				INT				IDENTITY,
-	usuario_id		INT				NOT NULL,
-	nome			VARCHAR(100)	NOT NULL,
-	rm				VARCHAR(10)		UNIQUE NOT NULL,
-	curso			VARCHAR(20)		NOT NULL,
-	formacao		VARCHAR(20)		NOT NULL, -- DEZEMBRO/2026
-	dataNascimento	DATE			NOT NULL,
-	dataCadastro	SMALLDATETIME	NOT NULL,
-	statusAluno		VARCHAR(20)		NOT NULL,
-
-	PRIMARY KEY (id),
-	FOREIGN KEY (usuario_id) REFERENCES Usuario (id)
-)
-GO
-INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusAluno)
-VALUES ('João Oliveira', 'rm12345', 'MTIzNDU2Nzg=', NULL, GETDATE(), 'INATIVO')
-
-INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusAluno)
-VALUES ('Cleiton dos Santos', 'rm12345','MTIzNDU2Nzg=', NULL,  GETDATE(), 'ATIVO')
-
-INSERT Aluno (nome, rm, senha, foto, dataCadastro, statusAluno)
-VALUES ('Jorge da Silva', 'rm12345', 'MTIzNDU2Nzg=', NULL, GETDATE(), 'ATIVO')
-
-GO
-
-
-
-
-GO
-
-CREATE TABLE Empresa(
-	id				INT				IDENTITY,
-	usuario_id		INT				NOT NULL,
-	nome			VARCHAR(100)	NOT NULL,
-	cnpj			VARCHAR(18)		UNIQUE NOT NULL,
-	complemento		VARCHAR(300)	NOT NULL,
-	cep				CHAR(8)			NOT NULL,
-	numero			VARCHAR(10)		NOT NULL,
-	webSite			VARCHAR(50)		NULL,
-	telefone		VARCHAR(20)		NOT NULL,
-	dataCadastro	SMALLDATETIME	NOT NULL,
-	statusEmpresa	VARCHAR(20)		NOT NULL,
-
-	PRIMARY KEY (id),
-	FOREIGN KEY (usuario_id) REFERENCES Usuario (id)
-
-)
-GO
-
-INSERT Empresa (nome, cnpj, complemento, cep, numero, webSite, telefone, dataCadastro, statusEmpresa)
-VALUES ('InovaTech', 'MTIzNDU2Nzg=', 'Inovatech@gmail.com', '11.222.333/0001-81', 'Avenida dos Girassóis N°200', GETDATE() , 'INATIVO')
-
-INSERT Empresa (nome, cnpj, complemento, cep, numero, webSite, telefone, dataCadastro, statusEmpresa)
-VALUES ('CodeStorm', 'MTIzNDU2Nzg=', 'CodeStorm@gmail.com', '11.222.333/0001-81', 'Avenida dos Girassóis N°200', GETDATE() , 'INATIVO')
-
-INSERT Empresa (nome, cnpj, complemento, cep, numero, webSite, telefone, dataCadastro, statusEmpresa)
-VALUES ('Biotec Pharma', 'MTIzNDU2Nzg=', 'Biotechpharma@gmail.com', '11.222.333/0001-81', 'Avenida dos Girassóis N°200', GETDATE() , 'INATIVO')
-
-
-SELECT * FROM Empresa
-
-SELECT * FROM Aluno
-
-CREATE TABLE Vaga(
+   id				INT				IDENTITY,
+   usuario_id		INT				NOT NULL,
+   nome				VARCHAR(100)	NOT NULL,
+   rm				VARCHAR(10)		UNIQUE NOT NULL,
+   curso			VARCHAR(20)		NOT NULL,
+   conclusao		VARCHAR(20)		NOT NULL, -- DEZEMBRO/2026
+   curriculo		VARBINARY(MAX)	NULL,
+   dataNascimento	DATE			NOT NULL,
+   dataCadastro		SMALLDATETIME	NOT NULL,
+   statusAluno		VARCHAR(20)		NOT NULL,
  
-	id				INT				IDENTITY,
-	empresa_id		INT				NOT NULL,
-	nome			VARCHAR(100)	NOT NULL,
-	descricao		VARCHAR(500)	NOT NULL,
-	cidade			VARCHAR(100)	NULL,
-	bairro			VARCHAR(100)	NULL,
-	cargaHoraria	VARCHAR(100)    NULL,
-	salario		    VARCHAR(100)	NULL,
-	area			VARCHAR(100)	NOT NULL,
-	dataCadastro	SMALLDATETIME	NOT NULL,
-	statusVaga		VARCHAR(20)		NOT NULL,
- 
-	PRIMARY KEY (id),
-	FOREIGN KEY (empresa_id) REFERENCES Empresa(id)
-
-)
+   PRIMARY KEY (id),
+   FOREIGN KEY (usuario_id) REFERENCES Usuario (id)
+);
 GO
  
-INSERT Vaga (nome, descricao, cidade,bairro, cargaHoraria, salario, area, statusVaga)
-VALUES ('Vaga x', 'Desenvolvedor web', 'rua Saturno 123', '30 horas', '600 reais', 'informática', '2025-2026', 'ATIVO')
-
-INSERT Vaga (nome, descricao, cidade,bairro, cargaHoraria, salario, area, statusVaga)
-VALUES ('Vaga y', 'Engenheiro de software', 'rua marte 123', '29 horas', '600 reais', 'informática', '2025-2026', 'ATIVO')
-
-INSERT Vaga (nome, descricao, cidade,bairro, cargaHoraria, salario, area, statusVaga)
-VALUES ('Vaga z', 'Desenvolvedor web', 'rua Urano 123', '35 horas', '600 reais', 'informática', '2025-2026', 'ATIVO')
+-- Inserir dados na tabela Aluno
+INSERT Aluno (usuario_id, nome, rm, curso, conclusao, dataNascimento, dataCadastro, statusAluno)
+VALUES 
+(1, 'João Oliveira', 'rm12345', 'Informática', 'DEZEMBRO/2026', '2006-08-15', GETDATE(), 'INATIVO'),
+(2, 'Cleiton dos Santos', 'rm12346', 'Informática', 'DEZEMBRO/2026', '2006-07-22', GETDATE(), 'ATIVO'),
+(3, 'Jorge da Silva', 'rm12347', 'Informática', 'DEZEMBRO/2026', '2006-09-10', GETDATE(), 'ATIVO')
+ 
+-- Criar a tabela Empresa
+CREATE TABLE Empresa
+(
+   id				INT				IDENTITY,
+   usuario_id		INT				NOT NULL,
+   nome				VARCHAR(100)	NOT NULL,
+   cnpj				VARCHAR(18)		UNIQUE NOT NULL,
+   informacao		VARCHAR(300)	NOT NULL,
+   cep				CHAR(8)			NOT NULL,
+   numero			VARCHAR(10)		NOT NULL,
+   webSite			VARCHAR(50)		NULL,
+   telefone			VARCHAR(20)		NOT NULL,
+   dataCadastro		SMALLDATETIME	NOT NULL,
+   statusEmpresa	VARCHAR(20)		NOT NULL,
+ 
+   PRIMARY KEY (id),
+   FOREIGN KEY (usuario_id) REFERENCES Usuario (id)
+);
+GO
+ 
+-- Inserir dados na tabela Empresa
+INSERT Empresa (usuario_id, nome, cnpj, informacao, cep, numero, webSite, telefone, dataCadastro, statusEmpresa)
+VALUES 
+(4,'InovaTech', '11.222.333/0001-81', 'Sede administrativa', '12345678', '200', 'https://inovatech.com', '11999990000', GETDATE(), 'INATIVO'),
+(5, 'CodeStorm', '11.222.333/0001-82', 'Escritório central', '12345678', '210', 'https://codestorm.com', '11988880000', GETDATE(), 'INATIVO'),
+(6, 'Biotec Pharma', '11.222.333/0001-83', 'Laboratório principal', '12345678', '220', 'https://biotecpharma.com', '11987770000', GETDATE(), 'INATIVO')
+ 
+-- Criar a tabela Vaga
+CREATE TABLE Vaga
+(
+   id				INT				IDENTITY,
+   empresa_id		INT				NOT NULL,
+   nome				VARCHAR(100)	NOT NULL,
+   descricao		VARCHAR(500)	NOT NULL,
+   cidade			VARCHAR(100)	NULL,
+   bairro			VARCHAR(100)	NULL,
+   cargaHoraria		VARCHAR(100)    NULL,
+   salario		    VARCHAR(100)	NULL,
+   area				VARCHAR(100)	NOT NULL,
+   dataCadastro		SMALLDATETIME	NOT NULL,
+   statusVaga		VARCHAR(20)		NOT NULL,
+ 
+   PRIMARY KEY (id),
+   FOREIGN KEY (empresa_id) REFERENCES Empresa(id)
+);
+GO
+ 
+-- Inserir dados na tabela Vaga
+INSERT Vaga (empresa_id, nome, descricao, cidade, bairro, cargaHoraria, salario, area, dataCadastro, statusVaga)
+VALUES 
+(1, 'Vaga X', 'Desenvolvedor web', 'São Paulo', 'Centro', '30 horas', '600 reais', 'Informática', GETDATE(), 'ATIVO'),
+(2, 'Vaga Y', 'Engenheiro de software', 'Campinas', 'Taquaral', '29 horas', '700 reais', 'Informática', GETDATE(), 'ATIVO'),
+(3, 'Vaga Z', 'Analista de sistemas', 'Santos', 'Ponta da Praia', '35 horas', '750 reais', 'Informática', GETDATE(), 'ATIVO')
  
  
-SELECT * FROM Vaga
+-- Criar a tabela PerfilAluno
+CREATE TABLE ContatoAluno
+(
+   id					INT				IDENTITY,
+   aluno_id				INT				NOT NULL,
+   nomeContato			VARCHAR(50)		NOT NULL,
+   link					VARCHAR(200)	NOT NULL,
  
+   PRIMARY KEY (id),
+   FOREIGN KEY (Aluno_id) REFERENCES Aluno (id)
+);
+GO
+ 
+-- Inserir dados na tabela PerfilAluno
+INSERT ContatoAluno(aluno_id, nomeContato, link)
+VALUES 
+('Informática', '2', '11900220000', 'estudante@email.com', 'Sem experiência', 'ATIVO'),
+('Informática para Internet', '3', '11913050000', 'estudante2@email.com', 'Serviço comunitário', 'INATIVO'),
+('Edificações', '1', '11924960000', 'estudante3@email.com', 'Participação na Olimpíada de Informática 2025', 'INATIVO')
 GO
 
-CREATE TABLE PerfilAluno(
-	id					INT				IDENTITY,
-	curso				VARCHAR(20)		NOT NULL,
-	ano					CHAR(1)			NOT NULL,
-	telefone			CHAR(11)		NOT NULL,
-	email				VARCHAR(30)		NOT NULL,
-	experiencia			VARCHAR(1000)	NOT NULL,
-	statusPerfil		VARCHAR(20)		NOT NULL,
-
-PRIMARY KEY (id)
-
-)
-
+CREATE TABLE Candidatura
+(
+   id					INT				IDENTITY,
+   aluno_id				INT				NOT NULL,
+   vaga_id				INT				NOT NULL,
+   data_cadastro		SMALLDATETIME	NOT NULL,
+   status_candidatura	VARCHAR(20)		NOT NULL,
+   
+ 
+   PRIMARY KEY (id),
+   FOREIGN KEY (aluno_id) REFERENCES Aluno (id),
+   FOREIGN KEY (vaga_id)  REFERENCES Vaga (id)
+);
 GO
-
-INSERT PerfilAluno (curso, ano, telefone, email, experiencia, statusPerfil)
-VALUES ('informática', '2', '11 90022000', 'estudante@email.com', 'sem experiência', 'ATIVO')
-
-INSERT PerfilAluno (curso, ano, telefone, email, experiencia, statusPerfil)
-VALUES ('irformática para Internet', '3', '11 91305000', 'estudante2@email.com', 'Serviço comunitário', 'INATIVO')
-
-INSERT PerfilAluno (curso, ano, telefone, email, experiencia, statusPerfil)
-VALUES ('Edficações', '1', '11 92496000', 'estudante3@email.com', 'Participação da Olímpiada de informática 2025', 'INATIVO')
-
-SELECT * FROM PerfilAluno
-
-CREATE TABLE Administrador(
-	id				INT				IDENTITY,
-	usuario_id		INT				NOT NULL,
-	nome			VARCHAR(100)	NOT NULL,
-	email			VARCHAR(30)		NOT NULL,
-	statusAdmin		VARCHAR(20)		NOT NULL,
-
-	PRIMARY KEY (id)
-
-)
-GO
-
-INSERT Administrador (nome, email, statusAdmin)
-VALUES ('Fulano da Silva', 'fulano@email.com.br', 'ATIVO')
-
-INSERT Administrador (nome, email, statusAdmin)
-VALUES ('João da Silva', 'joao@email.com.br', 'ATIVO')
-
-INSERT Administrador (nome, email, statusAdmin)
-VALUES ('Sicrana da Silva', 'sicrana@email.com.br', 'ATIVO')
-
-SELECT * FROM Administrador
+ 
+-- Selecionar dados das tabelas criadas
+SELECT * FROM Usuario;
+SELECT * FROM Aluno;
+SELECT * FROM Empresa;
+SELECT * FROM Vaga;
+SELECT * FROM ContatoAluno;
+SELECT * FROM Candidatura
